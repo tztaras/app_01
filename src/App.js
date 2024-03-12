f1f9171f6b443bc2434e5eb3d180da118a8f06df
tarasend <tz.taras@gmail.com>
2024-03-12 16:24:52 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -21,12 +21,14 @@ function App() {
 
       <h1>Greetings Universe!</h1>
       <p className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quo ullam laboriosam nihil, cumque dolores dolorum, nam necessitatibus tempora eveniet tempore commodi eaque est. Dolorum ea aliquam, error quas laboriosam amet accusantium assumenda fugit minima suscipit! Quos obcaecati sapiente odio eos quam earum debitis! Vel temporibus voluptatum quae nostrum odio.</p>
-      <hr/>
+      <hr />
+      <div>werwerwer</div>
       <select>
         <option value='green'>green</option>
         <option value='red'>red</option>
         <option value='cadetblue'>cadetblue</option>
         <option value='purple'>purple</option>
+        
       </select>
       <hr/>
       <img src='/img/side_eye_cat.jpg' height='200px' alt='side eye cat' title='side eye cat'/>
